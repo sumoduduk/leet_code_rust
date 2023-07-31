daf8f1e1298755e82954922211dec2461ec6aa3c
web6dev <calista@web6devs-MacBook-Air.local>
2023-07-31 13:46:18 +0700

    update

--- src/best_time_buy_stock.rs
+++ src/best_time_buy_stock.rs
@@ -1,47 +1,22 @@
-fn buy_n_sell(nums: &Vec<i32>) -> i32 {
-    let mut index_max: usize = 0;
-    let mut index_min: usize = 0;
-    let mut max: i32 = nums[0];
-    let mut min: i32 = nums[0];
-    let len = nums.len();
+fn buy_n_sell(prices: &Vec<i32>) -> i32 {
+    let mut profit = 0;
+    let mut buying = prices[0];
 
-    for (i, num) in nums.iter().enumerate() {
-        let max_inner = max.max(*num);
-        if max != max_inner {
-            max = max_inner;
-            index_max = i;
-        }
-
-        let min_inner = min.min(*num);
-        if min != min_inner {
-            if index_max > i {
-                min = min_inner;
-                index_min = i;
-            }
-        }
-
-        dbg!(i);
-        dbg!(num);
-        dbg!(index_min);
-        dbg!(min);
-        dbg!(index_max);
-        dbg!(max);
-        println!("\n");
+    for &price in prices.iter() {
+        profit = profit.max(price - buying);
+        buying = buying.min(price)
     }
 
-    if index_min < index_max {
-        max - min
-    } else {
-        0
-    }
+    profit
 }
 
 pub fn run() {
-    let nums = vec![2, 4, 10, 3, 1, 3, 4, 7, 11, 6, 0, 7];
-    // let nums = vec![3, 2, 6, 5, 0, 3];
+    let prices = vec![2, 4, 10, 3, 1, 3, 4, 7, 11, 6, 0, 7];
+    // let prices = vec![3, 2, 6, 5, 0, 3];
+    // let prices = vec![10, 0];
 
-    // let nums = vec![1, 4, 2];
-    dbg!(&nums);
-    let profit = buy_n_sell(&nums);
+    // let prices = vec![1, 4, 2];
+    // let prices = vec![2, 4, 1];
+    let profit = buy_n_sell(&prices);
     assert_eq!(10, profit)
 }
