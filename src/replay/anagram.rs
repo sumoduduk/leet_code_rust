2f1290eca890211803d3b7690214530042cfc816
sumoduduk <sumoduduk13@gmail.com>
2024-07-31 22:22:19 +0700

    optimize the code

--- src/replay/anagram.rs
+++ src/replay/anagram.rs
@@ -1,11 +1,23 @@
 fn is_anagram(s: String, t: String) -> bool {
-    let mut s_bytes = s.as_bytes().to_vec();
-    let mut t_bytes = t.as_bytes().to_vec();
+    let slen = s.len();
+    let tlen = t.len();
 
-    s_bytes.sort();
-    t_bytes.sort();
+    if tlen != slen {
+        return false;
+    }
 
-    s_bytes == t_bytes
+    let sbyte = s.as_bytes();
+    let tbyte = t.as_bytes();
+
+    let mut s_arr: [u16; 26] = [0; 26];
+    let mut t_arr: [u16; 26] = [0; 26];
+
+    for i in 0..tlen {
+        s_arr[(sbyte[i] - b'a') as usize] += 1;
+        t_arr[(tbyte[i] - b'a') as usize] += 1;
+    }
+
+    s_arr == t_arr
 }
 
 #[cfg(test)]
@@ -13,22 +25,42 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_anagram_1() {
+    fn test_anagram_replay_1() {
         let s = "anagram".to_string();
         let t = "nagaram".to_string();
 
         let res = is_anagram(s, t);
 
-        assert_eq!(res, true);
+        assert!(res);
     }
 
     #[test]
-    fn test_anagram_2() {
+    fn test_anagram_replay_2() {
         let s = "rat".to_string();
         let t = "car".to_string();
 
         let res = is_anagram(s, t);
 
-        assert_eq!(res, false);
+        assert!(!res);
+    }
+
+    #[test]
+    fn test_anagram_replay_3() {
+        let s = "ac".to_string();
+        let t = "bb".to_string();
+
+        let res = is_anagram(s, t);
+
+        assert!(!res);
+    }
+
+    #[test]
+    fn test_anagram_replay_4() {
+        let s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab".to_string();
+        let t = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbba".to_string();
+
+        let res = is_anagram(s, t);
+
+        assert!(!res);
     }
 }
